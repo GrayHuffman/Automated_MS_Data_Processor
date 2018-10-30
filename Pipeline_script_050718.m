d1cad304073d38fe2a12cbf895af271a610a93ba
Gray Huffman <40698035+GrayHuffman@users.noreply.github.com>
2018-10-30 16:38:21 -0400

    Add files via upload
    This is the Matlab automated processing script called at the end of the Powershell script

--- Pipeline_script_050718.m
+++ Pipeline_script_050718.m
@@ -3,17 +3,29 @@
 % Created: 5/7/18 (RGH)
 % Updated: 8/30/18
     % auto-email if last entry in imported files contains today's date
+% Requires:
+%   QEQC_viz.m
+%   QEQC_vizHelper.m
+%   QC_dates.m
+
+    
 
 global path
-path =  'C:/Google_Drive/MS/';
-addpath( genpath( [path 'bin/'] ) );
+%Add the path to your data files here
+path =  '____';
+%Add the additional location of your dependent Matlab scripts
+%Here it is assumed they are a subfolder in your initial path
+addpath( genpath( [path '____'] ) );
 
 % Email Prefs
-setpref('Internet','E_mail','QEQC.Reports@gmail.com');
+%Insert the sending email address here
+setpref('Internet','E_mail','_____');
 setpref('Internet','SMTP_Server','smtp.gmail.com');
 %setpref('Internet','E_mail','QEQC.Reports@gmail.com');
-setpref('Internet','SMTP_Username','QEQC.Reports');
-setpref('Internet','SMTP_Password','AutoMailer818');
+%Insert the username for your originating email address here
+setpref('Internet','SMTP_Username','______');
+%Insert the password for the originating email address here
+setpref('Internet','SMTP_Password','______');
 props = java.lang.System.getProperties;
 props.setProperty('mail.smtp.auth','true');
 props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
@@ -29,8 +41,9 @@ TodaysDate = [datestr((now), 'yymmdd')];
 QEQC_viz(timeFrame, TodaysDate);
 
 % Send e-mail report
-recipients = {'nslavov@northeastern.edu' 'a.koller@northeastern.edu' 'chen.alb@husky.neu.edu' 'e.emmott@northeastern.edu' 'specht.h@husky.neu.edu' 'huffman.r@husky.neu.edu'}
-%recipients = {'huffman.r@husky.neu.edu'}
+% Insert the recipient list for your automated report here
+recipients = {'you@gmail.com'}
+
 if (contains(timeFrame(end), TodaysDate))
-   sendmail(recipients,['QEQC Report:', TodaysDate],[ 'Good Morning!', char(10),char(10), ['A new QEQC Report is available at:',char(10) ,'https://web.northeastern.edu/slavov/qeqc/', TodaysDate, '/']]) ; 
+   sendmail(recipients,['QEQC Report:', TodaysDate],[ 'Good Morning!', char(10),char(10), ['A new QC Report is available!']]) ; 
 end
